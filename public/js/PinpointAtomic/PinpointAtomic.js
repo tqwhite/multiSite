4d6a5f2c71a4511c7200455a870e5c0d8d0e8a7d
TQ White II/qbook <tq@justkidding.com>
2012-09-29 10:10:04 -0500

    back to demo version with smaller kit

--- public/js/PinpointAtomic/PinpointAtomic.js
+++ public/js/PinpointAtomic/PinpointAtomic.js
@@ -1,8 +1,8 @@
 steal(
 '../jquery-1.6.4.js',
+'../pinpoint/resources/jqueryPlugins/jquery-ui-1.8.23.custom.min.js',
 './demo.js',
 
 function(){
-
-console.log('getting started');
+	window.pinpoint.init();
 })
