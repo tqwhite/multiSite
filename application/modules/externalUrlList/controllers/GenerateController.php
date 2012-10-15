68b342c356b4e10aa285d96fe4571875da95df38
TQ White II/qbook <tq@justkidding.com>
2012-10-15 11:05:33 -0500

    refactored headers and footers

--- application/modules/externalUrlList/controllers/GenerateController.php
+++ application/modules/externalUrlList/controllers/GenerateController.php
@@ -90,8 +90,77 @@ class ExternalUrlList_GenerateController extends Q_Controller_Base
 			'validatedEntity'=>$contentArray,
 			'source'=>__file__,
 			'propertyList'=>array(
+				array('name'=>'headNav.ini'),
+				array('name'=>'headBanner.html'),
 				array('name'=>'urlList.ini')
 			)));
+	}
+
+	public function initializeContentDirectory(){
+	$headNav=<<<HEADNAV
+
+menu.0.title="About Us"
+
+menu.0.links.0.title='Our Story'
+menu.0.links.0.url='#ourStory'
+
+menu.0.links.1.title='Contact Info'
+menu.0.links.1.url='#contactInfo'
+
+
+menu.1.title="Our Partners"
+
+menu.1.links.0.title='The Cambridge Group'
+menu.1.links.0.url='http://www.cambridgestrategics.com/'
+
+menu.1.links.1.title='Anoka-Henepin School District'
+menu.1.links.1.url='http://www.---.com/'
+
+HEADNAV;
+
+$siteDirectoryUrlList=<<<SITEDIR
+
+;if you do not want a site directory, leave the rest of this file blank
+a.0.title="Company List"
+
+a.0.links.0.title='Administrative Solutions'
+a.0.links.0.url='http://admin.cmerdc.local/sitemap'
+a.0.links.0.selector='.contentzone'	;this is a jQuery selector for the content to extract from target page
+
+a.0.links.1.title='Document Imaging'
+a.0.links.1.url='http://imaging.cmerdc.local/sitemap'
+a.0.links.1.selector='.contentzone'	;this is a jQuery selector for the content to extract from target page
+
+SITEDIR;
+
+$urlList=<<<URLLIST
+
+a.0.title="a 1"
+
+a.0.links.1.title='Overview'
+a.0.links.1.url='http://www.erdc.k12.mn.us/products/prdataw/index.php'
+a.0.links.1.selector='.contentzone'
+
+a.0.links.2.title='Contact'
+a.0.links.2.url='http://www.erdc.k12.mn.us/products/prdataw/dwcontac/index.php'
+a.0.links.2.selector='.contentzone'
+
+URLLIST;
+
+		$directories=array(
+			'ROOTFILES'=>array(  //ROOTFILES is the keyword for files that are peered at the base level of the route.
+				'contactFooter.html'=>"FOOTER: edit contactFooter.html to customize",
+				'headBanner.html'=>"HEAD BANNER: edit headBanner.html to customize",
+				'headNav.ini'=>$headNav,
+				'pageTitle.txt'=>"TITLE: edit pageTitle.txt to customize",
+				'siteDirectoryUrlList.ini'=>$siteDirectoryUrlList,
+				'urlList.ini'=>$urlList
+
+			)
+
+		);
+
+		$this->_helper->InitPageTypeDirectory($directories);
 	}
 
 }
