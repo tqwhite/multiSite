084df1f980674c1d8244a01aa6b9403f7cb38dd8
TQ White II/qbook <tq@justkidding.com>
2012-10-30 16:13:25 -0500

    added wilbert post test page

--- public/static/test/index.php
+++ public/static/test/index.php
@@ -1,37 +1,18 @@
-<?php $destinationAdr='tq@justkidding.com'; $extraMessage='hello'; require_once($_SERVER['DOCUMENT_ROOT'].'/php/mailNotificationInclude.php');?>
-<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01//EN' 'http://www.w3.org/TR/html4/strict.dtd'>
-<html>
-<head>
-	<META HTTP-EQUIV='Content-Type' CONTENT='text/html; charset=UTF-8'>
+<?php
 
-	<title>Template Page Title</title>
+if(count($_POST)<1){echo "\$_POST['json'] is EMPTY<BR/>\n\n"; exit;}
 
-	<script type='text/javascript' src='http://code.jquery.com/jquery-1.6.1.min.js'></script>
-	<!--link rel='stylesheet' type='text/css' href='css/main.css' /-->
+$transactionInfo=json_decode($_POST['transactionInfo']);
+$productList=json_decode($_POST['productList']);
 
-	<style type='text/css'><!--
-
-		body {color:orange;}
-
-	--></style>
-
-</head>
-<body>
-
-	Hello World
-
-</body>
-
-<script type='text/javascript'>
-	/* <![CDATA[ */
-	$(document).ready(function(){
-
-
-		$('body').append("<p>Thanks for visiting!");
-
-
-	});
-	/* ]]> */
-</script>
-
-</html>
+echo json_encode(
+	array(
+		'status'=>1,
+		'message'=>'test worked',
+		'data'=>array('token'=>$_POST['token'], 'echo'=>array(
+			'transactionInfo'=>$transactionInfo,
+			'productList'=>$productList,
+			'token'=>$_POST['token']
+		))
+	)
+);
