c55e34f67c76503fe7ab29230fdf6427f8a8759b
TQ White II <tq@justkidding.com>
2012-12-21 14:36:07 -0600

    production compile

--- library/Q/View/Helper/FilenameToLabel.php
+++ library/Q/View/Helper/FilenameToLabel.php
@@ -3,10 +3,15 @@
 
 	 public function filenameToLabel($filename) {
 
+		$extensionArray=array();
+		preg_match('/\\.(.*)$/', $filename, $extensionArray);
+		$extension=$extensionArray[0];
+
 	 	$elementArray=explode('_', $filename);
-	 	$elementArray=array_splice($elementArray, 1);
+
+	 	$elementArray=array_splice($elementArray, 1); //starts with 1 to omit sequencing prefix
 		$labelString=implode(' ', $elementArray);
-		$labelString=str_replace('.html', '', $labelString);
+		$labelString=str_replace($extension, '', $labelString);
 		return $labelString;
 	 }
 
