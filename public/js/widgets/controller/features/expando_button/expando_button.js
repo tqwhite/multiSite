fdbe75cd8f1b9aa5470696eb88b5891cde7abb44
TQ White II <tq@justkidding.com>
2013-02-08 09:43:35 -0600

    expandoButton, superGlobalItems

--- public/js/widgets/controller/features/expando_button/expando_button.js
+++ public/js/widgets/controller/features/expando_button/expando_button.js
@@ -12,35 +12,63 @@ Widgets.Controller.Base.extend('Widgets.Controller.Features.ExpandoButton',
 /** @Prototype */
 {
 	init : function(el, options){
+	
+//this will cause the parent of any button matched to expand/contract
+//works for multiple button/container sets per invocation
+
+		qtools.validateProperties({
+			targetObject:options,
+			targetScope: this, //will add listed items to targetScope
+			propList:[
+				{name:'buttonSelector'},
+				{name:'shrunkButtonClassName'},
+				{name:'expandedButtonClassName'}
+
+			],
+			source:this.constructor._fullName
+		});
+
 		
-		this.options=options;
-		
-		this.buttonObj=$(this.options.buttonSelector).click(this.callback('expandHandler'));
+		this.buttonObj=$(this.buttonSelector).click(this.callback('expandHandler'));
 
 		this.expandToggle='shrunk';
 
-// 		this.element.html("//widgets/controller/features/expando_button/views/init.ejs",{
-// 			message: "Hello World"
-// 		});
+		//this.element.html("//widgets/controller/features/expando_button/views/init.ejs",{
+		//message: "Hello World"
+		//});
 	},
 	
 	expandHandler:function(eventObj){
 		var target=$(eventObj.target)
 			parent=target.parent();
-AAA=parent;
-BBB=this;
+
 		if (this.expandToggle=='shrunk'){
-			target.removeClass(this.options.expandedButtonClassName).addClass(this.options.shrunkButtonClassName);
-			this.origHeight=target.parent().css('height');
-			parent.css({height:'100%'}, 5000);
+			target.removeClass(this.expandedButtonClassName).addClass(this.shrunkButtonClassName);
+			this.origHeight=target.parent().outerHeight();
+			
+			var expandedHeight=this.getExpandedHeight(parent);
+			
+			parent.animate({height:expandedHeight}, 500);
 			this.expandToggle='expanded';
 		}
 		else{
-			target.removeClass(this.options.shrunkButtonClassName).addClass(this.options.expandedButtonClassName);
-			parent.css({height:this.origHeight}, 5000);
+			target.removeClass(this.shrunkButtonClassName).addClass(this.expandedButtonClassName);
+			parent.animate({height:this.origHeight}, 500);
 			this.expandToggle='shrunk';
 		}
-	}
+		
+		this.turnOffClicksForAwhile(1000);
+	},
+
+getExpandedHeight:function(domObj){
+
+	var currHeight=domObj.height();
+	domObj.css({height:'100%'}); //for some happy reason, this does not display to user
+	var expandedHeight=domObj.height();
+	domObj.css({height:currHeight});
+	return expandedHeight;
+
+}
 })
 
 });
