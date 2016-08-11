8e30e6f63092c636ed4080049ee36e2c44e64b4c
TQ White II <tq@justkidding.com>
2016-08-10 23:23:04 -0500

    revised headNav (basicUnorderedList) so that, if there is no URL in the .ini, the title element is used intact. This allows things other than links to be used in the menu.

--- library/Q/View/Helper/UnorderedListForSelect.php
+++ library/Q/View/Helper/UnorderedListForSelect.php
@@ -17,14 +17,23 @@ private function makeOneMenu($menuSpec, $components){
 
 	 	$itemTemplate="<li><a href='<!url!>'><!title!></a></li>";
 	 	$blockTemplate="<ul class='<!mainClass!>'><li><a href='#'><!title!></a><ul><!linkString!></ul></li></ul>";
+		$passThroughTemplate="<li><!title!></li>";
+
 		$linkString='';
 
 		$blockTemplate=str_replace('<!mainClass!>', $components['mainClass'], $blockTemplate);
 
 		$title=$menuSpec['title'];
 		foreach ($menuSpec['links'] as $label=>$data){
+		if (isset($data['url'])){
 			$itemString=str_replace('<!title!>', $data['title'], $itemTemplate);
 			$itemString=str_replace('<!url!>', $data['url'], $itemString);
+		}
+		
+		else{
+			$itemString=str_replace('<!title!>', $data['title'], $passThroughTemplate);
+		}
+		
 			$linkString.=$itemString;
 		}
 
