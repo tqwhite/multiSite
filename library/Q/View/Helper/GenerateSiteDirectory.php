50258fc3b6a46590666a5657186556985ffb4695
TQ White II <tq@justkidding.com>
2019-03-14 16:54:07 -0500

    fixed problem with getting site directories

--- library/Q/View/Helper/GenerateSiteDirectory.php
+++ library/Q/View/Helper/GenerateSiteDirectory.php
@@ -1,49 +1,41 @@
 <?php
- class Q_View_Helper_GenerateSiteDirectory extends Zend_View_Helper_Abstract {
+class Q_View_Helper_GenerateSiteDirectory extends Zend_View_Helper_Abstract {
 
-	 public function generateSiteDirectory($urlIniList) {
+	public function generateSiteDirectory($urlIniList) {
 
 		return $this->scrapeList($urlIniList);
-	 }
+	}
 
+	private function scrapeList($inData) {
+		$outList = array();
+		foreach ($inData as $label => $group) {
+			foreach ($group as $label2 => $section) {
+				foreach ($section['links'] as $label3 => $linkItem) {
+					$tmp = $linkItem;
+					$tmp['content'] = $this->getContentZone($linkItem);
 
-
-    private function scrapeList($inData){
-		$outList=array();
-    	foreach ($inData as $label=>$group){
-			foreach ($group as $label2=>$section){
-				foreach($section['links'] as $label3=>$linkItem){
-						$tmp=$linkItem;
-					$tmp['content']=$this->getContentZone($linkItem);
-
-					$outList[]=$tmp;
-					}
+					$outList[] = $tmp;
+				}
 			}
 		}
 		return $outList;
-    }
+	}
 
-	private function getContentZone($inData){
-		$targetUrl=$inData['url'];
+	private function getContentZone($inData) {
+		error_log("TQ WARNING: Q_View_Helper_GenerateSiteDirectory->getContentZone() says, NEVER USES SELECTOR \$inData['selector'] ({$inData['selector']})");
 
-		$handler=curl_init($targetUrl);
- 		curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
- 		$rawPage=curl_exec($handler);
- 		curl_close($handler);
-		$dom = new Zend_Dom_Query($rawPage);
+		$targetUrl = $inData['url'];
+		$handler = curl_init($targetUrl);
+		curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+		$rawPage = curl_exec($handler);
+		curl_close($handler);
 
-		if (!$rawPage){return "$targetUrl is empty or missing<br/>";}
+		if (!$rawPage) {return "$targetUrl is empty or missing<br/>";}
 
-		if (isset($inData['selector'])){
-			$results = $dom->query($inData['selector']);
-			foreach ($results as $node) {
-				$nodeHtml=$results->getDocument()->saveXML($node);
-				if ($nodeHtml){break;} //verify that it found something
-			}
-		}
-		else{
-			$nodeHtml=$rawPage;
-		}
+		$matches = array();
+		$tmp = preg_match("/(<ul.*ul>)/i", $rawPage, $matches);
+		$nodeHtml = $matches[1];
+
 		return $nodeHtml;
 	}
- }
+}
---
+++ public/test.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01//EN' 'http://www.w3.org/TR/html4/strict.dtd'>
+<html>
+<head>
+	<META HTTP-EQUIV='Content-Type' CONTENT='text/html; charset=UTF-8'>
+	
+	<title>Ten Sigma Video Tester</title>
+	
+	<script type='text/javascript' src='http://code.jquery.com/jquery-1.6.1.min.js'></script>
+	<!--link rel='stylesheet' type='text/css' href='css/main.css' /-->
+	
+	<style type='text/css'><!--
+	
+		body {
+			color:#888;
+			background:#666;
+			font-family:sans-serif;
+		}
+		
+		a{
+			text-decoration:none;
+			font-weight:bold;
+			color:#333;
+		}
+		
+		a:hover{
+			color:#aaa;
+		}
+		
+	--></style>
+	
+</head>
+<body>
+
+
+<table style='width:100%;height:100%;'><tr><td align='center' valign='middle'>
+	<video controls poster='defaultPoster.png'>
+	  <source src="http://static.cmerdc.org/tenSigma/training/AddingAndAssigningStudents.ogg" type="video/ogg">
+	  <source src="http://static.cmerdc.org/tenSigma/training/AddingAndAssigningStudents.mp4" type="video/mp4">
+	  Your browser does not support the <code>video</code> element.
+	</video>
+</td></tr></table>
+
+</body>
+
+<script type='text/javascript'>
+	/* <![CDATA[ */
+	$(document).ready(function(){
+	
+	
+//		$('body').append("<p>Thanks for visiting!");
+	
+	
+	});
+	/* ]]> */
+</script>
+
+</html>
